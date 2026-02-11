3b40b0c7a2f1ace5fa718d61599925cde5ce0fc5
v-bohdan-v <vorobiov.bohdan@gmail.com>
2026-02-11 13:03:04 +0100

    Add some implementation into the parser

--- Parser.cs
+++ Parser.cs
@@ -1,3 +1,6 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
 class Parser
 {
     private int curr = 0;
@@ -8,27 +11,72 @@ class Parser
         _tokens = tokens;
     }
 
-    public void Primary()
+    private bool IsAtEnd() => curr >= _tokens.Count;
+
+    private Token Advance()
+    {
+        Token token = _tokens[curr];
+        curr++;
+        return token;
+    }
+
+    private Token Peek()
+    {
+        return _tokens[curr];
+    }
+
+    private bool Except(TokenType type)
+    {
+        return false;
+    }
+
+    private bool Match(TokenType type)
+    {
+        if (type == _tokens[curr].Type)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private Token PreviousToken()
+    {
+        return _tokens[curr - 1];
+    }
+
+    public Expr ParsePrimary()
+    {
+        if (Match(TokenType.TOK_INTEGER)) return new NumberLiteral(false, PreviousToken().Lexeme);
+        if (Match(TokenType.TOK_FLOAT)) return new NumberLiteral(true, PreviosToken().Lexeme);
+        if (Match(TokenType.TOK_LPAREN))
+        {
+            Expr expr = ParseExpr();
+            if (!Match(TokenType.TOK_RPAREN)) throw new Exception("The ')' is expected");
+            else return new Grouping(expr);
+        }
+        throw new Exception($"Unexpected token");
+    }
+
+    public Expr ParseUnary()
+    {
+        if (Match(TokenType.TOK_NOT) || Match(TokenType.TOK_MINUS) || Match(TokenType.TOK_PLUS))
+        {
+            TokenType op = PreviousToken();
+        }
+        return ParsePrimary();
+    }
+
+    public Expr ParseFactor()
+    {
+        return ParseUnary();
+    }
+
+    public void ParseTerm()
     {
 
     }
 
-    public void Unary()
-    {
-
-    }
-
-    public void Factor()
-    {
-
-    }
-
-    public void Term()
-    {
-
-    }
-
-    public void Expr()
+    public void ParseExpr()
     {
 
     }
